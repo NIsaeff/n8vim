4d1377bd8ab509576bd4318522f06990147befa2
n8dawg <nisaeff1@gmail.com>
2025-10-12 15:33:01 -0700

    added comment shortcut

--- init.lua
+++ init.lua
@@ -360,6 +360,32 @@ require('lazy').setup({
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+
+  -- Smart commenting support
+  {
+    'numToStr/Comment.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('Comment').setup({
+        -- LHS of toggle mappings in NORMAL mode
+        toggler = {
+          line = 'gcc',      -- Line-comment toggle keymap
+          block = 'gbc',     -- Block-comment toggle keymap
+        },
+        -- LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+          line = 'gc',       -- Line-comment keymap
+          block = 'gb',      -- Block-comment keymap
+        },
+        -- LHS of extra mappings
+        extra = {
+          above = 'gcO',     -- Add comment on the line above
+          below = 'gco',     -- Add comment on the line below
+          eol = 'gcA',       -- Add comment at the end of line
+        },
+      })
+    end,
+  },
   --[[
   {
     'anthropics/claude-code.nvim',
